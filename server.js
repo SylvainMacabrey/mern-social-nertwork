3305f1c781d45e87f98f4015b46f302158ab4d2b
SylvainMacabrey <sylvain.macabrey2@gmail.com>
2021-10-30 17:35:25 +0200

    backend du projet: crud user, crud post, authentification, jwt, upload image

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const userRoutes = require('./routes/user.routes');
+const postRoutes = require('./routes/post.routes');
+const { checkUser, requireAuth } = require('./middleware/auth.middleware');
+
+require('dotenv').config({path: './config/.env'});
+require('./config/database');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extented: true }));
+app.use(cookieParser());
+
+const corsOptions = {
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    'allowedHeaders': ['sessionId', 'Content-Type'],
+    'exposedHeaders': ['sessionId'],
+    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    'preflightContinue': false
+}
+app.use(cors(corsOptions));
+
+// jwt
+app.get('*', checkUser);
+app.get('/jwtid', requireAuth, (req, res) => {
+    res.status(200).send(res.locals.user._id);
+});
+
+//routes
+app.use('/api/user', userRoutes);
+app.use('/api/post', postRoutes);
+
+// server
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`)
+});
